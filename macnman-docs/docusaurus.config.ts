21071d929a9a3c42fdac09f8024f5b638e8e57ac
Macnman Technologies <134378411+MacnMan@users.noreply.github.com>
2025-12-14 16:26:19 +0530

    Update docusaurus.config.ts

--- macnman-docs/docusaurus.config.ts
+++ macnman-docs/docusaurus.config.ts
@@ -15,10 +15,8 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://macnman.vercel.app',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  url: 'https://macnman.com',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
